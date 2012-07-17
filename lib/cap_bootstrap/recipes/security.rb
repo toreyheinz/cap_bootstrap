b385f59aeff535e0cf0ec9abe3b0f8b82ef0071f
Steven Chanin <steven_chanin@alum.mit.edu>
2012-07-17 15:00:31 -0700

    namespace security recipe and add code to generate a secret token

--- lib/cap_bootstrap/recipes/security.rb
+++ lib/cap_bootstrap/recipes/security.rb
@@ -1,13 +1,34 @@
+require 'securerandom'
+
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :security do
-    desc "Setup a firewall with UFW"
-    task :setup_firewall, roles: :web do
-      run "#{sudo} apt-get -y install ufw"
-      run "#{sudo} ufw default deny"
-      run "#{sudo} ufw allow 22/tcp"
-      run "#{sudo} ufw allow 80/tcp"
-      run "#{sudo} ufw allow 443/tcp"
-      run "#{sudo} ufw --force enable"
+    namespace :install do
+    desc "Install and configure a firewall with UFW"
+      task :firewall, roles: :web do
+        run "#{sudo} apt-get -y install ufw"
+        run "#{sudo} ufw default deny"
+        run "#{sudo} ufw allow 22/tcp"
+        run "#{sudo} ufw allow 80/tcp"
+        run "#{sudo} ufw allow 443/tcp"
+        run "#{sudo} ufw --force enable"
+      end
+    end
+
+    namespace :setup do
+      desc "Create a secret_token file if one doesn't exist"
+      task :secret_token, roles: :web do
+        run "mkdir -p #{shared_path}/config"
+
+        secret_token_path = "#{shared_path}/config/secret_token.rb"
+        unless remote_file_exists?(secret_token_path)
+          template "secret_token.erb", secret_token_path
+        end
+      end
+
+      desc "Symlink the secret token into the new release"
+      task :symlink, roles: :web do
+        run "ln -nfs #{shared_path}/config/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
+      end
     end
   end
 end
