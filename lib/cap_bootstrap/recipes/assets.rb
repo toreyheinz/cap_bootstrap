12b99846ca9c04e772b748e6c2af92f2e8f91467
Torey Heinz <toreyheinz@gmail.com>
2012-08-23 21:25:00 -0400

    Adding skip assets compile recipe

---
+++ lib/cap_bootstrap/recipes/assets.rb
@@ -0,0 +1,12 @@
+# http://www.bencurtis.com/2011/12/skipping-asset-compilation-with-capistrano/
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets | wc -l").to_i > 0
+        logger.info "Compiling Assets..."
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
